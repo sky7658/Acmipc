3a1b8262fecda1eeb197236f938e19d3e062dcef
sky7658 <jin721003@naver.com>
2022-11-07 20:16:29 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 220 ms, Memory: 12360 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int visit[100001];
+int cnt = 1;
+vector<int> arr[100001];
+
+void dfs(int v, int e, int r) {
+	visit[r] = cnt++;
+	for(int i = 0; i < arr[r].size(); i++) {
+		if(visit[arr[r][i]] == 0) dfs(v, e, arr[r][i]);
+	}
+}
+
+int main() {
+	int n, m, r, u, v;
+	cin >> n >> m >> r;
+	for(int i = 0; i < m; i++) {
+		cin >> u >> v;
+		arr[u].push_back(v);
+		arr[v].push_back(u);
+	}
+	for(int i = 1; i <= n; i++) {
+		sort(arr[i].begin(), arr[i].end());
+	}
+	dfs(n, m, r);
+	for(int i = 1; i <= n; i++) {
+		cout << visit[i] << '\n';
+	}
+	return 0;
+}
