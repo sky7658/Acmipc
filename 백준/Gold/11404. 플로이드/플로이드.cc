30656e67e13a35865a8083a7bc72fb5c86126374
sky7658 <jin721003@naver.com>
2023-04-16 15:46:47 +0900

    [Gold IV] Title: 플로이드, Time: 172 ms, Memory: 3104 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#define MAX 1234567891234
+using namespace std;
+
+int n, m, a, b, c;
+long long dist[101];
+vector<vector<pair<int, int>>> bus(101);
+
+void Dijkstra(int s) {
+    for (int i = 1; i <= n; i++) dist[i] = MAX;
+    priority_queue<pair<long long, int>> q;
+    q.push({0, s});
+    dist[s] = 0;
+
+    while (!q.empty()) {
+        int x = q.top().second;
+        long long w = -q.top().first;
+        q.pop();
+
+        for (int i = 0; i < bus[x].size(); i++) {
+            int nextX = bus[x][i].first;
+            int nextW = bus[x][i].second;
+
+            if (dist[nextX] <= w + nextW) continue;
+
+            dist[nextX] = w + nextW;
+            q.push({-dist[nextX], nextX});
+        }
+    }
+}
+
+int main(void) {
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b >> c;
+        bus[a].push_back({b, c});
+    }
+
+    for (int i = 1; i <= n; i++) {
+        Dijkstra(i);
+        for (int j = 1; j <= n; j++) {
+            if (dist[j] == MAX)
+                cout << 0 << ' ';
+            else
+                cout << dist[j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
