6fb31ac4691e75c1401d7882c855d22285db36c2
sky7658 <jin721003@naver.com>
2023-04-16 16:50:48 +0900

    [Gold IV] Title: 플로이드, Time: 88 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,54 +1,41 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 #define MAX 1234567891234
 using namespace std;
 
-int n, m, a, b, c;
-long long dist[101];
-vector<vector<pair<int, int>>> bus(101);
-
-void Dijkstra(int s) {
-    for (int i = 1; i <= n; i++) dist[i] = MAX;
-    priority_queue<pair<long long, int>> q;
-    q.push({0, s});
-    dist[s] = 0;
-
-    while (!q.empty()) {
-        int x = q.top().second;
-        long long w = -q.top().first;
-        q.pop();
-
-        for (int i = 0; i < bus[x].size(); i++) {
-            int nextX = bus[x][i].first;
-            int nextW = bus[x][i].second;
-
-            if (dist[nextX] <= w + nextW) continue;
-
-            dist[nextX] = w + nextW;
-            q.push({-dist[nextX], nextX});
-        }
-    }
-}
+int n, m, a, b;
+long long c;
+long long dist[101][101];
 
 int main(void) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
     cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j)
+                dist[i][j] = 0;
+            else
+                dist[i][j] = MAX;
+        }
+    }
 
     for (int i = 0; i < m; i++) {
         cin >> a >> b >> c;
-        bus[a].push_back({b, c});
+        dist[a][b] = min(dist[a][b], c);
+    }
+
+    for (int p = 1; p <= n; p++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][p] + dist[p][j]);
+            }
+        }
     }
 
     for (int i = 1; i <= n; i++) {
-        Dijkstra(i);
         for (int j = 1; j <= n; j++) {
-            if (dist[j] == MAX)
+            if (dist[i][j] == MAX)
                 cout << 0 << ' ';
             else
-                cout << dist[j] << ' ';
+                cout << dist[i][j] << ' ';
         }
         cout << '\n';
     }
