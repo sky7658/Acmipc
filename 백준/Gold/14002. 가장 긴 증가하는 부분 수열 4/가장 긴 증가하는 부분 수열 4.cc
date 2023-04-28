d15da66d71fdf67c91c9961c40f3c2360a056952
sky7658 <jin721003@naver.com>
2023-04-28 15:48:07 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,50 +1,43 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int n;
-int arr[1000];
-int dp[1000][2];
-vector<int> v;
+int arr[1000], dp[1000];
+vector<int> v, ans;
 
 int main(void) {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        dp[i][1] = -1;
     }
 
-    dp[0][0] = 1;
+    v.push_back(arr[0]);
+    dp[0] = 1;
 
-    int max_len = 1;
-    int max_index = 0;
-
+    int cnt = 1;
     for (int i = 1; i < n; i++) {
-        int temp = 1;
-        for (int j = i - 1; j >= 0; j--) {
-            if (arr[i] > arr[j]) {
-                if (temp < dp[j][0] + 1) {
-                    temp = dp[j][0] + 1;
-                    dp[i][1] = j;
-                }
-            }
-            dp[i][0] = temp;
-        }
-        if (max_len < dp[i][0]) {
-            max_len = dp[i][0];
-            max_index = i;
+        if (v.back() < arr[i]) {
+            v.push_back(arr[i]);
+            cnt++;
+            dp[i] = cnt;
+        } else {
+            int index = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
+            v[index] = arr[i];
+            dp[i] = index + 1;
         }
     }
-
-    cout << max_len << '\n';
-
-    while (true) {
-        v.push_back(arr[max_index]);
-        if (dp[max_index][1] == -1) break;
-        max_index = dp[max_index][1];
+    cout << v.size() << '\n';
+    for (int i = n - 1; i >= 0; i--) {
+        if (cnt == dp[i]) {
+            ans.push_back(arr[i]);
+            cnt--;
+        }
     }
-
-    for (auto itr = v.rbegin(); itr != v.rend(); itr++) cout << *itr << ' ';
-
+    while (!ans.empty()) {
+        cout << ans.back() << ' ';
+        ans.pop_back();
+    }
     return 0;
 }
