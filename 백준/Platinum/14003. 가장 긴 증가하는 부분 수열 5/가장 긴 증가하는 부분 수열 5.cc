bdd47c932c4eebf5fc9e74d2c1f4a9a7280cc0b3
sky7658 <jin721003@naver.com>
2023-04-28 15:49:15 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 652 ms, Memory: 22136 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int arr[1000000], dp[1000000];
+vector<int> v, ans;
+
+int main(void) {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    v.push_back(arr[0]);
+    dp[0] = 1;
+
+    int cnt = 1;
+    for (int i = 1; i < n; i++) {
+        if (v.back() < arr[i]) {
+            v.push_back(arr[i]);
+            cnt++;
+            dp[i] = cnt;
+        } else {
+            int index = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
+            v[index] = arr[i];
+            dp[i] = index + 1;
+        }
+    }
+    cout << v.size() << '\n';
+    for (int i = n - 1; i >= 0; i--) {
+        if (cnt == dp[i]) {
+            ans.push_back(arr[i]);
+            cnt--;
+        }
+    }
+    while (!ans.empty()) {
+        cout << ans.back() << ' ';
+        ans.pop_back();
+    }
+    return 0;
+}
