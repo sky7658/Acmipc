51a2969b7c0a8b829a1f9cb62d16a2dde67f86b2
sky7658 <jin721003@naver.com>
2023-04-29 16:57:21 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int lcs[1001][1001];
+string a, b;
+
+int main(void) {
+    cin >> a >> b;
+    for (int i = 1; i <= a.length(); i++) {
+        for (int j = 1; j <= b.length(); j++) {
+            if (a[i - 1] == b[j - 1]) {
+                lcs[i][j] = lcs[i - 1][j - 1] + 1;
+            } else {
+                lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1]);
+            }
+        }
+    }
+    cout << lcs[a.length()][b.length()];
+    return 0;
+}
