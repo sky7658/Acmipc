4dd78c904f8b302696f0e1e8744197ac1ff2e278
sky7658 <jin721003@naver.com>
2023-05-01 17:17:53 +0900

    [Gold IV] Title: DSLR, Time: 3884 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int t, a, b;
+bool visit[10000];
+
+void bfs() {
+    for (int i = 0; i < 10000; i++) {
+        visit[i] = false;
+    }
+
+    queue<pair<int, string>> q;
+    q.push({a, ""});
+    visit[a] = true;
+
+    while (!q.empty()) {
+        int x = q.front().first;
+        string str = q.front().second;
+        q.pop();
+
+        if (x == b) {
+            cout << str << '\n';
+            return;
+        }
+
+        int nextX;
+
+        nextX = (x * 2) % 10000;
+        if (!visit[nextX]) {
+            q.push({nextX, str + "D"});
+            visit[nextX] = true;
+        }
+
+        nextX = x - 1 < 0 ? 9999 : x - 1;
+        if (!visit[nextX]) {
+            q.push({nextX, str + "S"});
+            visit[nextX] = true;
+        }
+
+        nextX = (x % 1000) * 10 + x / 1000;
+        if (!visit[nextX]) {
+            q.push({nextX, str + "L"});
+            visit[nextX] = true;
+        }
+
+        nextX = x / 10 + (x % 10) * 1000;
+        if (!visit[nextX]) {
+            q.push({nextX, str + "R"});
+            visit[nextX] = true;
+        }
+    }
+}
+
+int main(void) {
+    cin >> t;
+    while (t-- > 0) {
+        cin >> a >> b;
+        bfs();
+    }
+
+    return 0;
+}
