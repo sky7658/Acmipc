11c13835ef7d87c4ee28238b7b8f2cbb3ce4b5cd
sky7658 <jin721003@naver.com>
2026-01-07 19:04:51 +0900

    [Silver I] Title: 줄어들지 않아, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.cc
+++ 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.cc
@@ -3,26 +3,22 @@ using namespace std;
 
 int main()
 {
-	long long dp[65][9] = { 0 ,};
+	long long dp[65][10] = { 0 ,};
 	int t, n;
 
-	dp[0][0] = 1;
-	
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		dp[1][i] = 9 - i;
+		dp[0][i] = 1;
 	}
 
-	for (int i = 2; i < 65; i++)
+	for (int i = 1; i <= 64; i++)
 	{
-		for (int j = 0; j < 9; j++)
+		for (int j = 0; j < 10; j++)
 		{
-			dp[i][0] += dp[i - 1][j];
-		}
-
-		for (int j = 1; j < 9; j++)
-		{
-			dp[i][j] = dp[i][j - 1] - dp[i - 1][j - 1];
+			for (int k = j; k < 10; k++)
+			{
+				dp[i][j] += dp[i - 1][k];
+			}
 		}
 	}
 
@@ -30,15 +26,8 @@ int main()
 
 	for (int i = 0; i < t; i++)
 	{
-		long long ans = 10;
 		cin >> n;
-
-		for (int i = 1; i < n; i++)
-		{
-			ans += dp[i + 1][0];
-		}
-
-		cout << ans << '\n';
+		cout << dp[n][0] << '\n';
 	}
 	return 0;
 }
